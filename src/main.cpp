f1cee266a799f4f8fee580433527e8beff2c3253
steven-swanbeck <steven.swanbeck@gmail.com>
2023-02-24 16:03:56 -0600

    submission

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-/*  Arduino DC Motor Control - PWM | H-Bridge | L298N
-         Example 02 - Arduino Robot Car Control
-    by Dejan Nedelkovski, www.HowToMechatronics.com
-*/
 #include <Arduino.h>
 
 // Motor Driver Connections
@@ -12,18 +8,9 @@
 #define in4 6
 #define enB 5
 #define button 4
-
+// Definitions
 int motorSpeedA = 0;
 int motorSpeedB = 0;
-
-// Joystick Connections
-#define joyX A1
-#define joyY A2
-
-// Potentiometer Connections
-#define pot_pin A0
-
-// misc variables
 int rot_direction {};
 int button_pressed {};
 
@@ -34,87 +21,157 @@ void setup() {
     pinMode(in2, OUTPUT);
     pinMode(in3, OUTPUT);
     pinMode(in4, OUTPUT);
-
-    // initial motor directions
-    //   setMotorsForward();
     Serial.begin(9600);
+
+    motorSpeedA = 170;
+    motorSpeedB = 240;
+
+    // User input
+    // -------------------
+    int width {1}; // in
+    int height {1}; // in
+    bool direction {0}; // 0 for left, 1 for right
+    // -------------------
+    float linear_speed {14.5}; //in/s
+    float t_wid {width / linear_speed};
+    float t_hgt {height / linear_speed};
+    float t_turn {(PI/2)/linear_speed};
+    delay(3000);
+    int count = 0;
+
+    //pull up to spot
+    while(count < 7){
+        long unsigned int t {millis()};
+        long unsigned int t_beg2 {millis()};
+        t = t_beg2;
+        while (t-t_beg2<t_wid*1000){
+            motorSpeedA = 200;
+            motorSpeedB = 240;
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, HIGH);
+            digitalWrite(in4, LOW);
+            t = millis();
+            analogWrite(enA, motorSpeedA); 
+            analogWrite(enB, motorSpeedB);
+        }
+        count = count+1;
+        Serial.println(count);
+    }
+    count = 0;
+    if (direction == true) {
+        //pivot counter clockwise 90 degrees    
+        while(count < 6){
+            long unsigned int t {millis()};
+            long unsigned int t_beg2 {millis()};
+            t = t_beg2;
+            while (t-t_beg2<t_turn*1000){
+                motorSpeedA = 200;
+                motorSpeedB = 0;
+                digitalWrite(in1, HIGH);
+                digitalWrite(in2, LOW);
+                digitalWrite(in3, LOW);
+                digitalWrite(in4, HIGH);
+                t = millis();
+                analogWrite(enA, motorSpeedA); 
+                analogWrite(enB, motorSpeedB);
+            }
+            count = count+1;
+            Serial.println(count);
+        }
+    } else {
+        //pivot clockwise 90 degrees    
+        while(count < 6){
+            long unsigned int t {millis()};
+            long unsigned int t_beg2 {millis()};
+            t = t_beg2;
+            while (t-t_beg2<t_turn*1000){
+                motorSpeedA = 0;
+                motorSpeedB = 240;
+                digitalWrite(in1, HIGH);
+                digitalWrite(in2, LOW);
+                digitalWrite(in3, LOW);
+                digitalWrite(in4, HIGH);
+                t = millis();
+                analogWrite(enA, motorSpeedA); 
+                analogWrite(enB, motorSpeedB);
+            }
+            count = count+1;
+            Serial.println(count);
+        }
+    }
+    count = 0;
+    //reverse into spot
+    while(count < 1){
+        long unsigned int t {millis()};
+        long unsigned int t_beg2 {millis()};
+        t = t_beg2;
+        while (t-t_beg2<t_wid*1000){
+            motorSpeedA = 200;
+            motorSpeedB = 240;
+            digitalWrite(in1, HIGH);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+            t = millis();
+            analogWrite(enA, motorSpeedA); 
+            analogWrite(enB, motorSpeedB);
+        }
+        count = count+1;
+        Serial.println(count);
+    }
+    count = 0;
+    if (direction == true) {
+        //rotate clockwise 90 degreess
+        while(count < 6){
+            long unsigned int t {millis()};
+            long unsigned int t_beg2 {millis()};
+            t = t_beg2;
+            while (t-t_beg2<t_wid*1000){
+                motorSpeedA = 150;
+                motorSpeedB = 190;
+                digitalWrite(in1, LOW);
+                digitalWrite(in2, HIGH);
+                digitalWrite(in3, LOW);
+                digitalWrite(in4, HIGH);
+                t = millis();
+                analogWrite(enA, motorSpeedA); 
+                analogWrite(enB, motorSpeedB);
+            }
+            count = count+1;
+            Serial.println(count);
+        }
+    } else {
+        //rotate clockwise 90 degreess
+        while(count < 6){
+            long unsigned int t {millis()};
+            long unsigned int t_beg2 {millis()};
+            t = t_beg2;
+            while (t-t_beg2<t_wid*1000){
+                motorSpeedA = 150;
+                motorSpeedB = 190;
+                digitalWrite(in1, HIGH);
+                digitalWrite(in2, LOW);
+                digitalWrite(in3, HIGH);
+                digitalWrite(in4, LOW);
+                t = millis();
+                analogWrite(enA, motorSpeedA); 
+                analogWrite(enB, motorSpeedB);
+            }
+            count = count+1;
+            Serial.println(count);
+        }
+    }
+    
+    if (count>5){
+        motorSpeedA = 0;
+        motorSpeedB = 0;
+        analogWrite(enA, motorSpeedA);
+        analogWrite(enB, motorSpeedB);
+    }
 }
 
 void loop() {
-    int xAxis = analogRead(A1); // Read Joysticks X-axis
-    int yAxis = analogRead(A2); // Read Joysticks Y-axis
-    Serial.println(xAxis);
-    Serial.println(yAxis);
-
-
-  // Y-axis used for forward and backward control
-    if (yAxis > 530) {
-    // Set Motor A backward
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        // Set Motor B backward
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
-        motorSpeedA = map(yAxis, 530, 1023, 0, 255);
-        motorSpeedB = map(yAxis, 530, 1023, 0, 255);
-    }
-    else if (yAxis < 480) {
-    // Set Motor A forward
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        // Set Motor B forward
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-        motorSpeedA = map(yAxis, 480, 0, 0, 255);
-        motorSpeedB = map(yAxis, 480, 0, 0, 255);
-    }
-  // If joystick stays in middle the motors are not moving
-    else {
-        motorSpeedA = 0;
-        motorSpeedB = 0;
-    }
-
-  // X-axis used for left and right control
-    if (xAxis < 480) {
-        // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
-        int xMapped = map(xAxis, 480, 0, 0, 255);
-        // Move to left - decrease left motor speed, increase right motor speed
-        motorSpeedA = motorSpeedA - xMapped;
-        motorSpeedB = motorSpeedB + xMapped;
-        // Confine the range from 0 to 255
-        if (motorSpeedA < 0) {
-            motorSpeedA = 0;
-        }
-        if (motorSpeedB > 255) {
-            motorSpeedB = 255;
-        }
-    }
-    if (xAxis > 530) {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
-        int xMapped = map(xAxis, 530, 1023, 0, 255);
-        // Move right - decrease right motor speed, increase left motor speed
-        motorSpeedA = motorSpeedA + xMapped;
-        motorSpeedB = motorSpeedB - xMapped;
-        // Confine the range from 0 to 255
-        if (motorSpeedA > 255) {
-            motorSpeedA = 255;
-        }
-        if (motorSpeedB < 0) {
-            motorSpeedB = 0;
-        }
-    }
-    // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-    if (motorSpeedA < 70) {
-        motorSpeedA = 0;
-    }
-    if (motorSpeedB < 70) {
-        motorSpeedB = 0;
-    }
-    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+    analogWrite(enA, 0);
+    analogWrite(enB, 0);
 }
-
-
-
