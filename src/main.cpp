434fa2cf7d002730ebef60ec24a128d163fbf201
steven-swanbeck <steven.swanbeck@gmail.com>
2023-02-27 09:19:46 -0600

    upkeep

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@
 // Definitions
 int motorSpeedA = 0;
 int motorSpeedB = 0;
-int rot_direction {};
-int button_pressed {};
 
 void setup() {
     pinMode(enA, OUTPUT);
@@ -23,30 +21,33 @@ void setup() {
     pinMode(in4, OUTPUT);
     Serial.begin(9600);
 
-    motorSpeedA = 170;
-    motorSpeedB = 240;
+    motorSpeedA = 0;
+    motorSpeedB = 0;
 
     // User input
     // -------------------
-    int width {1}; // in
-    int height {1}; // in
-    bool direction {0}; // 0 for left, 1 for right
+    int WallDist {9}; // in (optimal 9)
+    bool direction {0}; // 0 for left, 1 for right    
     // -------------------
+
     float linear_speed {14.5}; //in/s
-    float t_wid {width / linear_speed};
-    float t_hgt {height / linear_speed};
+    float t_pullup {1.2 / linear_speed}; //
+    float t_rot1 {.9 / linear_speed}; //
+    float t_reverse {((WallDist-3) / linear_speed)}; //
+    float t_rot2 {.8/ linear_speed};
     float t_turn {(PI/2)/linear_speed};
+    
     delay(3000);
     int count = 0;
 
     //pull up to spot
-    while(count < 7){
+    while(count <6){
         long unsigned int t {millis()};
         long unsigned int t_beg2 {millis()};
         t = t_beg2;
-        while (t-t_beg2<t_wid*1000){
+        while (t-t_beg2<t_pullup*1000){
             motorSpeedA = 200;
-            motorSpeedB = 240;
+            motorSpeedB = 220;
             digitalWrite(in1, LOW);
             digitalWrite(in2, HIGH);
             digitalWrite(in3, HIGH);
@@ -60,53 +61,55 @@ void setup() {
     }
     count = 0;
     if (direction == true) {
-        //pivot counter clockwise 90 degrees    
-        while(count < 6){
-            long unsigned int t {millis()};
-            long unsigned int t_beg2 {millis()};
-            t = t_beg2;
-            while (t-t_beg2<t_turn*1000){
-                motorSpeedA = 200;
-                motorSpeedB = 0;
-                digitalWrite(in1, HIGH);
-                digitalWrite(in2, LOW);
-                digitalWrite(in3, LOW);
-                digitalWrite(in4, HIGH);
-                t = millis();
-                analogWrite(enA, motorSpeedA); 
-                analogWrite(enB, motorSpeedB);
-            }
-            count = count+1;
-            Serial.println(count);
-        }
-    } else {
-        //pivot clockwise 90 degrees    
-        while(count < 6){
-            long unsigned int t {millis()};
-            long unsigned int t_beg2 {millis()};
-            t = t_beg2;
-            while (t-t_beg2<t_turn*1000){
-                motorSpeedA = 0;
-                motorSpeedB = 240;
-                digitalWrite(in1, HIGH);
-                digitalWrite(in2, LOW);
-                digitalWrite(in3, LOW);
-                digitalWrite(in4, HIGH);
-                t = millis();
-                analogWrite(enA, motorSpeedA); 
-                analogWrite(enB, motorSpeedB);
-            }
-            count = count+1;
-            Serial.println(count);
-        }
+      //rotate counterclockwise 90 degrees (right)
+      while(count < 5){
+          long unsigned int t {millis()};
+          long unsigned int t_beg2 {millis()};
+          t = t_beg2;
+          while (t-t_beg2<t_rot1*1000){
+              motorSpeedA = 200;
+              motorSpeedB = 240;
+              digitalWrite(in1, HIGH);
+              digitalWrite(in2, LOW);
+              digitalWrite(in3, HIGH);
+              digitalWrite(in4, LOW);
+              t = millis();
+              analogWrite(enA, motorSpeedA); 
+              analogWrite(enB, motorSpeedB);
+          }
+          count = count+1;
+          Serial.println(count);
+      }
     }
+    else {
+      //rotate clockwise 90 degrees (left)
+      while(count < 5){
+          long unsigned int t {millis()};
+          long unsigned int t_beg2 {millis()};
+          t = t_beg2;
+          while (t-t_beg2<t_rot1*1000){
+              motorSpeedA = 200;
+              motorSpeedB = 240;
+              digitalWrite(in1, LOW);
+              digitalWrite(in2, HIGH);
+              digitalWrite(in3, LOW);
+              digitalWrite(in4, HIGH);
+              t = millis();
+              analogWrite(enA, motorSpeedA); 
+              analogWrite(enB, motorSpeedB);
+          }
+          count = count+1;
+          Serial.println(count);
+      }
+    }
+    
     count = 0;
     //reverse into spot
     while(count < 1){
         long unsigned int t {millis()};
         long unsigned int t_beg2 {millis()};
         t = t_beg2;
-        while (t-t_beg2<t_wid*1000){
+        while (t-t_beg2<t_reverse*1000){
             motorSpeedA = 200;
             motorSpeedB = 240;
             digitalWrite(in1, HIGH);
@@ -122,56 +125,56 @@ void setup() {
     }
     count = 0;
     if (direction == true) {
-        //rotate clockwise 90 degreess
-        while(count < 6){
-            long unsigned int t {millis()};
-            long unsigned int t_beg2 {millis()};
-            t = t_beg2;
-            while (t-t_beg2<t_wid*1000){
-                motorSpeedA = 150;
-                motorSpeedB = 190;
-                digitalWrite(in1, LOW);
-                digitalWrite(in2, HIGH);
-                digitalWrite(in3, LOW);
-                digitalWrite(in4, HIGH);
-                t = millis();
-                analogWrite(enA, motorSpeedA); 
-                analogWrite(enB, motorSpeedB);
-            }
-            count = count+1;
-            Serial.println(count);
-        }
-    } else {
-        //rotate clockwise 90 degreess
-        while(count < 6){
-            long unsigned int t {millis()};
-            long unsigned int t_beg2 {millis()};
-            t = t_beg2;
-            while (t-t_beg2<t_wid*1000){
-                motorSpeedA = 150;
-                motorSpeedB = 190;
-                digitalWrite(in1, HIGH);
-                digitalWrite(in2, LOW);
-                digitalWrite(in3, HIGH);
-                digitalWrite(in4, LOW);
-                t = millis();
-                analogWrite(enA, motorSpeedA); 
-                analogWrite(enB, motorSpeedB);
-            }
-            count = count+1;
-            Serial.println(count);
-        }
+      //rotate clockwise 90 degreess (right)
+      while(count < 6){
+          long unsigned int t {millis()};
+          long unsigned int t_beg2 {millis()};
+          t = t_beg2;
+          while (t-t_beg2<t_rot2*1000){
+              motorSpeedA = 200;
+              motorSpeedB = 220;
+              digitalWrite(in1, LOW);
+              digitalWrite(in2, HIGH);
+              digitalWrite(in3, LOW);
+              digitalWrite(in4, HIGH);
+              t = millis();
+              analogWrite(enA, motorSpeedA); 
+              analogWrite(enB, motorSpeedB);
+          }
+          count = count+1;
+          Serial.println(count);
+      }
+    }
+    else{
+      //rotate counterclockwise 90 degreess (left)
+      while(count < 6){
+          long unsigned int t {millis()};
+          long unsigned int t_beg2 {millis()};
+          t = t_beg2;
+          while (t-t_beg2<t_rot2*1000){
+              motorSpeedA = 200;
+              motorSpeedB = 220;
+              digitalWrite(in1, HIGH);
+              digitalWrite(in2, LOW);
+              digitalWrite(in3, HIGH);
+              digitalWrite(in4, LOW);
+              t = millis();
+              analogWrite(enA, motorSpeedA); 
+              analogWrite(enB, motorSpeedB);
+          }
+          count = count+1;
+          Serial.println(count);
+      }
     }
     
-    if (count>5){
-        motorSpeedA = 0;
-        motorSpeedB = 0;
-        analogWrite(enA, motorSpeedA);
-        analogWrite(enB, motorSpeedB);
+    motorSpeedA = 0;
+    motorSpeedB = 0;
+    analogWrite(enA, motorSpeedA);
+    analogWrite(enB, motorSpeedB);
     }
-}
+
 
 void loop() {
     analogWrite(enA, 0);
-    analogWrite(enB, 0);
+    analogWrite(enB, 0);    
 }
